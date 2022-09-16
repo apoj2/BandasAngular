bd3c7c9a0c3b60b49f2d767afd19127bac6bd8f0
Apoj2 <angeltobonperez5@gmail.com>
2022-09-16 11:21:58 -0500

    CreamosArchivoBeta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlbumesComponent } from './albumes/albumes.component';
+import { HomeComponent } from './home/home.component'
+import { IntegrantesComponent } from './integrantes/integrantes.component';
+import { HistoriaComponent } from './historia/historia.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent,pathMatch:'full'},
+  {path:'integrantes',component:IntegrantesComponent},
+  {path:'albumes',component:AlbumesComponent},
+  {path:'history',component:HistoriaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { IntegrantesComponent } from './integrantes/integrantes.component';
+import { AlbumesComponent } from './albumes/albumes.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { HistoriaComponent } from './historia/historia.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntegrantesComponent,
+    AlbumesComponent,
+    MenuComponent,
+    FooterComponent,
+    HomeComponent,
+    HistoriaComponent,
   ],
   imports: [
     BrowserModule,
